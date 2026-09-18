1e7b450cfc7ff0264334220e1a6d73dfd619d170
Jorge Mora <89847502+jmora123@users.noreply.github.com>
2026-09-18 14:01:09 +0200

    CallToAction.js update

--- src/CallToAction.js
+++ src/CallToAction.js
@@ -8,8 +8,8 @@ function CallToAction() {
         <h1>Little Lemon</h1>
         <h2>Chicago</h2>
         <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-        <Link to="/booking">
-          <button className="btn-primary">Reserve a Table</button>
+        <Link to="/booking" className="btn-primary">
+          Reserve a Table
         </Link>
       </div>
       <div className="hero-image">
